7a9dc673cd7a93a9dd61c0ba8ccdf6d8ff0548df
pimentazil <1matheuspimentel@gmail.com>
2024-02-29 21:08:08 -0300

    subindo tudo isso dai mlk

---
+++ WebApiBooks.Repository/Models/TabLivro.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiBooks.Repository.Models
+{
+    public class TabLivro
+    {
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public string autor { get; set; }
+        public decimal preco { get; set; }
+        public DateTime dataLancamento { get; set; }
+    }
+}
